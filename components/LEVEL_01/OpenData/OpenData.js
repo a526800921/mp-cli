cc3a3a3def537195d2609c3df5d199ddf7726388
DESKTOP-SUP6C2C\Administrator <526800921@qq.com>
2018-05-28 09:52:50 +0800

    新增Open

---
+++ components/LEVEL_01/OpenData/OpenData.js
@@ -0,0 +1,36 @@
+// components/LEVEL_01/OpenData/OpenData.js
+const { _wx, _Component } = getApp().globalData
+
+_Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    type: {
+      type: String,
+      value: 'userNickName'
+    },
+    openGid: {
+      type: String,
+      value: ''
+    },
+    lang: {
+      type: String,
+      value: 'en'
+    },
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
